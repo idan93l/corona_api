023ed8f377607ebdb94156c34d5ac5c2ea26e13c
Idan Lev <idan.93lev@gmail.com>
2021-12-14 00:30:46 +0200

    corona

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ const data = {
       fill: true,
       backgroundColor: gradient,
       borderColor: "#fff",
-      pointBackgroundColor: "#e63946",
+      pointBackgroundColor: ['#003f5c','#2f4b7c','#665191','#a05195','#d45087','#f95d6a','#ff7c43','#ffa600'],
       tension: 0.3,
     },
   ],
